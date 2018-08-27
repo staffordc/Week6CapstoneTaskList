629ac052d6a537445451c8bce9cd380c7478fb28
staffordc <stafford.carh@gmail.com>
2018-08-27 07:57:24 -0400

    non-zero amount of css

--- Week6CapstoneTaskList/Controllers/HomeController.cs
+++ Week6CapstoneTaskList/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Week6CapstoneTaskList.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "What's this about?";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your Microsoft fake contact page.";
 
             return View();
         }
